0c9313a90715f630fc1b66283adae1581bbabcb5
mohammedsalah7 <mhamad_m@windowslive.com>
2021-07-13 16:24:48 +0300

    add defalut data items #22

---
+++ Js/data.js
@@ -0,0 +1,59 @@
+const data = [
+    {
+        id: 0, 
+        name: "Hijab",
+        details: "New Style Hijab",
+        price: 25,
+        image:"",
+      },
+      {
+        id: 2, 
+        name: "Hijab",
+        details: "New Style Hijab",
+        price: 25,
+        image:"",
+      },
+      {
+        id: 3, 
+        name: "Hijab",
+        details: "New Style Hijab",
+        price: 25,
+        image:"",
+      },
+      {
+        id: 4, 
+        name: "Hijab",
+        details: "New Style Hijab",
+        price: 25,
+        image:"",
+      },
+      {
+        id: 5, 
+        name: "Hijab",
+        details: "New Style Hijab",
+        price: 25,
+        image:"",
+      },
+      {
+        id: 6, 
+        name: "Hijab",
+        details: "New Style Hijab",
+        price: 25,
+        image:"",
+      },
+      {
+        id: 7, 
+        name: "Hijab",
+        details: "New Style Hijab",
+        price: 25,
+        image:"",
+      },
+      {
+        id: 8, 
+        name: "Hijab",
+        details: "New Style Hijab",
+        price: 25,
+        image:"",
+      },
+
+]
