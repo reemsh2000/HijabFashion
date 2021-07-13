1f30844f0e997d56edd3f9a1b9477f50a0e3c97b
reemsh2000 <sh951652@gmail.com>
2021-07-13 19:35:29 +0300

    add to cart func

---
+++ Js/cart.js
@@ -0,0 +1,37 @@
+
+// Get Data from local storage to print it in Cart page 
+function getDataFromLocalSorage(){
+    let dataFromLocal = JSON.parse(localStorage.getItem("data"));
+    console.log(dataFromLocal)
+    for(var z=0; dataFromLocal.length >z  ;++z){
+      // Create item in html with classes
+        let item=document.createElement('div')
+        itemsContainer.appendChild(item)
+        item.classList.add='item'
+        let imgDiv=document.createElement('div')
+        item.appendChild(imgDiv)
+        imgDiv.classList.add='img'
+        let img=document.createElement('img')
+        imgDiv.appendChild(img)
+        let price=document.createElement('p')
+        imgDiv.appendChild(price)
+        let delBtn=document.createElement('button')
+        item.appendChild(delBtn)
+        delBtn.classList.add='deleteBtn'
+        //  Edit Element Content
+        let index = getData(dataFromLocal[z])
+        img.src=data[index].image
+        price=data[index].price
+        delBtn.setAttribute("id",data[index].id);
+        delBtn.textContent='Remove'
+    }
+  }
+     //  Get Data by id 
+     function getData (id){
+      for(var i=0;i<data.length;i++){
+        if(data[i].id==id)
+        break;
+        return i;
+     }
+      
+   }
--- Js/main.js
+++ Js/main.js
@@ -1,33 +1,25 @@
 const itemsContainer=document.querySelector('.items')
 
 function addToCart(id){
-  // Create item in html with classes
-  let item=document.createElement('div')
-  itemsContainer.appendChild(item)
-  item.classList.add='item'
-  let imgDiv=document.createElement('div')
-  item.appendChild(imgDiv)
-  imgDiv.classList.add='img'
-  let img=document.createElement('img')
-  imgDiv.appendChild(img)
-  let price=document.createElement('p')
-  imgDiv.appendChild(price)
-  let delBtn=document.createElement('button')
-  item.appendChild(delBtn)
-  delBtn.classList.add='deleteBtn'
-//    Edit Element Content
-let index = getData(id)
-   img.src=data[index].image
-   price=data[index].price
-   delBtn.setAttribute("id",data[index].id);
-   delBtn.textContent='Remove'
+  let dataArray = [];         // Array to push id which add to cart  
+
+  function addNameToLocalSorage(){
+  const dataObject = {
+      itemId: id,
+  }
+  /* If there is data saved already in local storage, add the new data to old data*/
+  let oldData = JSON.parse(localStorage.getItem("itemsId"));
   
-}
-function getData (id){
- for(var i=0;i<data.length;i++){
-     if(data[i].id==id)
-     break;
-     return i;
- }
-   
+  if((oldData !== null)){
+      oldData.push(dataObject);
+      localStorage.setItem("itemsId", JSON.stringify(oldData))
+     
+  } else{     /* If local storage is empty, Push new data to the empty array */
+      dataArray.push(dataObject)  //Push object of data to the array
+      /* set stringified data in local storage */
+      localStorage.setItem('itemsId', JSON.stringify(dataArray))
+  }
+  
+  }
+  
 }
