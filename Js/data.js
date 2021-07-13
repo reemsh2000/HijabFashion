e450227b18afe0b69b921040d10e965dd7aeff45
mohammedsalah7 <mhamad_m@windowslive.com>
2021-07-13 16:46:57 +0300

    add category items  #22

--- Js/data.js
+++ Js/data.js
@@ -5,6 +5,7 @@ const data = [
         details: "New Style Hijab",
         price: 25,
         image:"https://www.zyadda.com/wp-content/uploads/2021/04/hijab_jan_23_photoshoot0400_1.jpg",
+        category :"Dress soiree"
       },
       {
         id: 1, 
@@ -12,6 +13,8 @@ const data = [
         details: "New Style Hijab",
         price: 25,
         image:"https://www.sayidaty.net/sites/default/files/styles/800x510/public/2020/06/20/6820411-1854158242.jpg",
+        category :"Formal"
+
       },
       {
         id: 2, 
@@ -19,6 +22,8 @@ const data = [
         details: "New Style Hijab",
         price: 25,
         image:"https://images-na.ssl-images-amazon.com/images/I/418oAXVrsjL._AC_SY580_.jpg",
+        category :"dress"
+
       },
       {
         id: 3, 
@@ -26,6 +31,8 @@ const data = [
         details: "New Style Hijab",
         price: 25,
         image:"https://www.aljamila.com/sites/default/files/styles/1100x732_scale/public/2019/10/30/3051451-1682286252.jpg",
+        category :"casual"
+
       },
       {
         id: 4, 
@@ -33,27 +40,35 @@ const data = [
         details: "New Style Hijab",
         price: 25,
         image:"https://www.sayidaty.net/sites/default/files/styles/800x510/public/2020/10/28/7135861-841127383.jpg",
-      },
+        category :"Formal"
+
+    },
       {
         id: 5, 
         name: "Hijab",
         details: "New Style Hijab",
         price: 25,
-        image:"",
-      },
+        image:"https://5jl.cc/wp-content/uploads/2019/11/126-12.jpg",
+        category :"dress"
+
+    },
       {
         id: 6, 
         name: "Hijab",
         details: "New Style Hijab",
         price: 25,
         image:"https://img.yasmina.com/08eEeGOJrR8pICYuUDfH89DKNqE=/700x792/smart/http://harmony-assets-live.s3.amazonaws.com/image_source/a5/67/a567a4df4f7293c0951a6439ef25fe8d0671ce10.jpg",
-      },
+        category :"Dress soiree"
+
+    },
       {
         id: 7, 
         name: "Hijab",
         details: "New Style Hijab",
         price: 25,
         image:"https://www.mexatk.com/wp-content/uploads/2016/09/%D8%AD%D8%AC%D8%A7%D8%A8-%D8%AA%D8%B1%D9%83%D9%8A-%D8%B4%D9%8A%D9%83-1.jpg",
+        category :"casual"
+
       },
 
 ]
