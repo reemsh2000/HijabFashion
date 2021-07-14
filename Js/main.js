516c229e3608960eeb636dc1111e65659f0dc7e1
reemsh2000 <sh951652@gmail.com>
2021-07-14 05:36:11 +0300

    make required edits

--- Js/main.js
+++ Js/main.js
@@ -1,13 +1,9 @@
-const data =require('./data.js')
-// const searchInput=document.querySelector('.search-box')
-//Search By Name
-// searchInput.addEventListener('keypress', searchByName(searchInput.value))
-const searchByName =((search)=>{
-  const SearchedArray= data.filter((ele)=>{
-   if (ele.name==search)
-      return ele 
-  })
-//  return  displayData(SearchedArray)
+const data=require('./data.js')
+//Search By Name Function
+const searchByName =((searchName)=>{
+  const SearchedArray= data.filter((ele)=> ele.name==searchName)
+  //  return  displayItems(SearchedArray)
   return SearchedArray
-})
+  })
+
 module.exports =searchByName
--- Js/test.js
+++ Js/test.js
@@ -1,23 +1,26 @@
 const searchByName = require('./main.js');
 const data =require('./data.js')
+
+// Search By Name Function test 
 test('should return all the objects contain the pass name',()=>{
-    const actual=searchByName('Formal')
-    const expected=[ {
+    const actual=searchByName('dress')
+    const expected=[ 
+      {
+        id: 0, 
+        name: "dress",
+        details: "New Style Hijab",
+        price: 25,
+        image:"https://www.zyadda.com/wp-content/uploads/2021/04/hijab_jan_23_photoshoot0400_1.jpg",
+        category :"Dress soiree"
+      },
+      {
         id: 1, 
-        name: "Hijab",
+        name: "dress",
         details: "New Style Hijab",
         price: 25,
         image:"https://www.sayidaty.net/sites/default/files/styles/800x510/public/2020/06/20/6820411-1854158242.jpg",
         category :"Formal"
 
-      },    {
-        id: 4, 
-        name: "Hijab",
-        details: "New Style Hijab",
-        price: 25,
-        image:"https://www.sayidaty.net/sites/default/files/styles/800x510/public/2020/10/28/7135861-841127383.jpg",
-        category :"Formal"
-
-    }]
+      }]
     expect(actual).toEqual(expected)
 })
