e93e47c743ffa9d426b26c7c284c5321ebd0001f
reemsh2000 <sh951652@gmail.com>
2021-07-23 22:48:23 +0300

    search by name func

--- Js/dom.js
+++ Js/dom.js
@@ -2,5 +2,12 @@ const container = document.querySelector(".items-container");
 
 const searchInput = document.querySelector(".search-box");
 displayItems();
-const addToCartClicked = reply_click();
-console.log(addToCartClicked);
+// ********************************************
+// Action to search when user click enter key  
+searchInput.addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+    let   searchProduct=searchInput.value;
+    let selectedData=searchByProductsName(searchProduct, data)
+    console.log(selectedData)
+    }
+});
